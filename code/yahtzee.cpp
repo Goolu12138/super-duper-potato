603b3e5af3c062eefbcd73d998658b6caa87c85d
Goolu12138 <1400110956@qq.com>
2024-07-28 23:38:02 +0800

    添加两次投掷骰子机会

--- code/yahtzee.cpp
+++ code/yahtzee.cpp
@@ -17,32 +17,70 @@ public:
     }
 
     std::vector<int> rollMultiple(int numberOfDice) {
-        std::vector<int> results;
-        for (int i; i <numberOfDice; i++ )
+        std::vector<int> results(numberOfDice);
+        for (int& result : results)
         {
-            results.push_back(roll());
+            result = roll();
         }
         return results;
     }
 
-    viod reroll(){
-        for (){
-            roll();
+    void reroll(std::vector<int> &dice, const std::vector<bool>& rerollFlags){
+        for (size_t i = 0;i < dice.size(); ++i){
+            if (rerollFlags[i])
+                dice[i] = roll();
         }
     }
 };
 
+void printDice(const std::vector<int> &dice){
+    for (int die:dice)
+        std::cout << die << " ";
+    std::cout << std::endl;
+}
+
 int main() {
     // 创建一个Dice对象
     Dice dice;
+    const int numberOfDice = 5;
+    int maxRerolls = 2;
 
     // 掷骰子并输出结果
-    std::vector<int> results = dice.rollMultiple(5);
-    std::cout << "你掷出的骰子是: " ;
-    for (int result : results)
-        std::cout << result << " ";
-    std::cout << std::endl;
+    std::vector<int> results = dice.rollMultiple(numberOfDice);
+    std::cout << "第一次骰子为: " ;
+    printDice(results);
 
+    for(int rerollCount=0; rerollCount < maxRerolls; ++rerollCount)
+    {
+        std::vector<bool> rerollFlags(numberOfDice, false);
+        char choice;
+        std::cout << "是否重新掷骰子?(y/n): ";
+        std::cin >> choice;
+
+        if (choice == 'n')
+            break;
+        
+        std::cout << "输入要重新掷的骰子的编号 (1-" << numberOfDice << "), 用空格分隔: ";
+        int dieIndex;
+        while(std::cin >> dieIndex)
+        {
+            if (dieIndex >= 1 && dieIndex <= numberOfDice)
+            {
+                rerollFlags[dieIndex - 1] = true;
+            }
+            if (std::cin.peek() == '\n') {
+                break;
+            }
+        }
+
+        dice.reroll(results, rerollFlags);
+        std::cout << "重掷骰子为："; 
+        printDice(results);
+         
+    }
+
+    std::cout << "最终骰子为: ";
+    printDice(results);
     return 0;
 }
 
